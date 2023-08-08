a974de2147d694cebab3e3a234eef0d446a5eb7a
qpxdesign <qroshan5@gmail.com>
2023-08-08 17:53:02 -0400

    protected from undefined data

--- src/components/CalenderSquare.js
+++ src/components/CalenderSquare.js
@@ -1,6 +1,14 @@
 import React from "react";
 import wmo_codes from "../assets/wmo-codes.json";
 export default function CalenderSquare(props) {
+  if (
+    props.data.weathercode === undefined ||
+    props.data.temperature_2m_max === undefined ||
+    props.data.temperature_2m_min === undefined ||
+    props.data.snowfall_sum === undefined
+  ) {
+    return <></>;
+  }
   return (
     <div className="box">
       <h5
@@ -14,17 +22,19 @@ export default function CalenderSquare(props) {
       >
         {props.date}
       </h5>
-      <img src={wmo_codes[props.data.weathercode[props.date - 1]]?.day.image} />
+      <img
+        src={wmo_codes[props.data?.weathercode[props.date - 1]]?.day.image}
+      />
       <h2>
-        {wmo_codes[props.data.weathercode[props.date - 1]]?.day.description}
+        {wmo_codes[props.data?.weathercode[props.date - 1]]?.day.description}
       </h2>
       <h2>
-        {props.data.temperature_2m_max[props.date - 1]}F /{" "}
-        {props.data.temperature_2m_min[props.date - 1]}F
+        {props.data?.temperature_2m_max[props.date - 1]}F /{" "}
+        {props.data?.temperature_2m_min[props.date - 1]}F
       </h2>
-      {props.data.snowfall_sum[props.date - 1] > 0.5 ? (
+      {props.data?.snowfall_sum[props.date - 1] > 0.5 ? (
         <h2 style={{ fontWeight: "100", fontSize: "1.2em", marginTop: 0 }}>
-          {Math.round(props.data.snowfall_sum[props.date - 1] * 100) / 100}in
+          {Math.round(props.data?.snowfall_sum[props.date - 1] * 100) / 100}in
           snow
         </h2>
       ) : (
